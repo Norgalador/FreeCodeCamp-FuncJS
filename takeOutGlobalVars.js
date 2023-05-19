245800e48e705aa73c460df38d56f25096603442
dojo-JoshBurlile <josh.burlile@gmail.com>
2023-05-18 18:06:42 -0700

    added mapMethod

---
+++ takeOutGlobalVars.js
@@ -0,0 +1,26 @@
+// 
+
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(arr, bookName) {
+  let newArray = [...arr];
+  newArray.push(bookName);
+  return newArray;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(arr, bookName) {
+  let newArray = [...arr];
+  const book_index = newArray.indexOf(bookName);
+  if (book_index >= 0) {
+
+    newArray.splice(book_index, 1);
+    return newArray;
+
+    // Change code above this line
+    }
+}
